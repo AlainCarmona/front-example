c53a23f40338971bd97bc970c16647b5b903de3c
AlainCarmona <alaincarmonahdz@gmail.com>
2019-01-14 13:35:08 -0600

    First commit

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.use(express.static('public'));
 app.get('/index.htm', function (req, res) {
    res.sendFile( __dirname + "/" + "index.htm" );
 })
-
+/*
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+*/
